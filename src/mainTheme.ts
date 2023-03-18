1ee92f05deed050d560d27caa301436ef459260a
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2023-03-18 12:26:15 +0100

    changed played main theme

--- src/mainTheme.ts
+++ src/mainTheme.ts
@@ -5,7 +5,7 @@ function playMainTheme() {
     let clicked = 0;
     const soundBtn = document.querySelector('.sound') as HTMLButtonElement;
     const soundImg = document.querySelector('#sound') as HTMLImageElement;
-    const audio = new Audio('https://ia800504.us.archive.org/33/items/TetrisThemeMusic/Tetris.mp3');
+    const audio = new Audio('https://upload.wikimedia.org/wikipedia/commons/e/e8/Korobeiniki.ogg');
     soundBtn.addEventListener('click', () => {
         if(clicked % 2 === 0) {
             clicked++;
